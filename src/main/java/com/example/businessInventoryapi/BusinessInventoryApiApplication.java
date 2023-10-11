462ab1ae2f1e15c1afa8f9b6b8124e7508ddc19c
DeveAlbert <albertemmanuel29@gmail.com>
2023-10-11 18:03:12 +0100

    pushed code from local machine

---
+++ src/main/java/com/example/businessInventoryapi/BusinessInventoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.businessInventoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessInventoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessInventoryApiApplication.class, args);
+	}
+
+}
